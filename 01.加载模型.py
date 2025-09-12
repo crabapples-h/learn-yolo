7ab94a409a061f4bf40f26ae8f6f10c5a38d47b5
mshe <666666666@666666666.666666666>
2025-09-12 22:39:02 +0800

    add requirements

--- 01.加载模型.py
+++ 01.加载模型.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 yolo = YOLO('models/yolov8n.pt')
 # yolo('./resources/01.png',show=True,save=True)
 # yolo('./resources/FHC09594.jpg',show=True,save=True)
-yolo('./resources/input.mp4',show=True,save=True)
+yolo('./resources/02.jpg',show=True,save=True)
